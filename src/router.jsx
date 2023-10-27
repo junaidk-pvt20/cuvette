7945c7d902dbca5575e830080c06e4c5bf90a11b
Junaid Khan <junaid@nimapinfotech.com>
2023-10-27 16:16:11 +0530

    Changes Done

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,9 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import { lazy } from "react";
 
 const LazyLayout = lazy(() => import("./layout/Index"));
-const LazyDashboard = lazy(() => import("./pages/dashboard"));
-const LazySkillTest = lazy(() => import("./pages/skilltest/Index"));
-const LazyInternship = lazy(() => import("./pages/internship"));
+const LazyDashboard = lazy(() => import("./pages/dashboard/Index.jsx"));
+const LazySkillTest = lazy(() => import("./pages/skilltest/Index.jsx"));
+const LazyInternship = lazy(() => import("./pages/internship/Index.jsx"));
 
 export const router = createBrowserRouter([
     {
